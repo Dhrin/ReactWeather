c2b46d0085aeaf33ca4d9f9f34f4ebeac5c68d76
Dhrin Anantamongkolchai <dhrin_IMAC@Dhrins-iMac.local>
2016-12-31 04:01:56 +0700

    Edit examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,5 +1,28 @@
 var React = require('react');
 
+
+
+var {Link} = require('react-router');
+
+var Examples = (props) => {
+    return (
+        <div>
+            <h1 className="text-center">Examples</h1>
+            <p className="text-center">Here are a few example location to try out:</p>
+            <ol>
+                <li>
+                    <Link to='/?location=Bangkok'>Bangkok, TH</Link>
+                </li>
+                <li>
+                    <Link to='/?location=Rio'>Rio, Brazil</Link>
+                </li>
+            </ol>
+        </div>
+    )
+};
+
+module.exports = Examples;
+
 //var Examples = React.createClass({
 //	render: function () {
 //		return (
@@ -8,15 +31,3 @@ var React = require('react');
 //
 //	}
 //});
-
-var Examples = (props) => {
-    return (
-        <div>
-            <h3>Examples</h3>
-            <p>Welcome to examples page!</p>
-        </div>
-    )
-};
-
-module.exports = Examples;
-
