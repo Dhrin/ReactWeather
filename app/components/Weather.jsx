8cbf4712a1545d1c19a37d128a46386aa9a04d4e
Dhrin Anantamongkolchai <dhrin_IMAC@Dhrins-iMac.local>
2017-01-01 02:57:58 +0700

    Fix search

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -17,7 +17,9 @@ var Weather = React.createClass({
         
         this.setState({
             isLoading: true,
-            errorMessage: undefined
+            errorMessage: undefined,
+            location: undefined,
+            temp: undefined
         });
         
         openWeatherMap.getTemp(location).then(function (temp) {
@@ -29,10 +31,30 @@ var Weather = React.createClass({
         }, function (e){
             that.setState({
                 isLoading: false,
-                errorMessage: e.message
+                errorMessage: e.message,
+                location: undefined,
+                temp: undefined
             });
         });
         
+    },
+    
+    componentDidMount: function () {
+        var location = this.props.location.query.location;
+        
+        if (location && location.length > 0) {
+            this.handleSearch(location);
+            window.location.hash = '#/';
+        }
+    },
+    
+    componentWillReceiveProps: function (newProps) {
+        var location = newProps.location.query.location;
+        
+        if (location && location.length > 0) {
+            this.handleSearch(location);
+            window.location.hash = '#/';
+        }
     },
     
 	render: function () {
